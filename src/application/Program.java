f8b11aa696d27e6571295995db990adddd37c923
Fernando Lopes <fernando._lopes@outlook.com>
2023-08-09 21:53:46 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,17 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+			
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department obj = new Department(1, "books");
-		Seller seller = new Seller(21, "Fernando", "fernando@outlook.com", new Date(), 3000.0, obj);
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
 	}
 }
