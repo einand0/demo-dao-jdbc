3ca0c6f4688b96020fdae1a2916cdb7d184eaaaf
Fernando Lopes <fernando._lopes@outlook.com>
2023-08-10 13:29:26 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,8 +10,8 @@ public class Program {
 			
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("TESTE N1 == seller findById");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 	}
 }
